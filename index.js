0a7ccef2c854541f604152464ff7f58bb991f975
Emdadul <emdad@solvrz.com>
2023-12-14 13:10:12 +0600

    Update charts layout and add timeline chart

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function analyzeMatrix(matrixValues, b) {
 
   showChart(X, Y, Z);
   showIteration();
+  showTimeline();
 
   console.log(jacobi_itr + " < > " + gauss_itr + " < > " + crammer_itr + " < > " + inverse_itr );
 }
@@ -475,5 +476,37 @@ function showIteration(){
 
   var chart = new ApexCharts(document.querySelector("#pieChart"), options);
   chart.render();
+
+}
+
+
+function showTimeline(){
+  var options = {
+    series: [{
+    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+  }],
+    chart: {
+    type: 'bar',
+    height: 350
+  },
+  plotOptions: {
+    bar: {
+      borderRadius: 4,
+      horizontal: true,
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  xaxis: {
+    categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
+      'United States', 'China', 'Germany'
+    ],
+  }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#barChart"), options);
+  chart.render();
+
 
 }
