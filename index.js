67c3a7d4172dc2f9abf0928d6a5e504850816c8a
Emdadul <emdad@solvrz.com>
2023-12-14 12:41:00 +0600

    feat: pie chart added

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ function analyzeMatrix(matrixValues, b) {
   }
 
   showChart(X, Y, Z);
+  showIteration();
 }
 
 function crammer(det, B) {
@@ -412,4 +413,51 @@ function showChart(X, Y, Z) {
 
   var chart = new ApexCharts(document.getElementById("myChart"), options);
   chart.render();
+}
+
+
+function showIteration(){
+     
+  var options = {
+    series: [44, 55, 41, 17, 15],
+    chart: {
+    width: 380,
+    type: 'donut',
+  },
+  plotOptions: {
+    pie: {
+      startAngle: -90,
+      endAngle: 270
+    }
+  },
+  dataLabels: {
+    enabled: false
+  },
+  fill: {
+    type: 'gradient',
+  },
+  legend: {
+    formatter: function(val, opts) {
+      return val + " - " + opts.w.globals.series[opts.seriesIndex]
+    }
+  },
+  title: {
+    text: 'Gradient Donut with custom Start-angle'
+  },
+  responsive: [{
+    breakpoint: 480,
+    options: {
+      chart: {
+        width: 200
+      },
+      legend: {
+        position: 'bottom'
+      }
+    }
+  }]
+  };
+
+  var chart = new ApexCharts(document.querySelector("#pieChart"), options);
+  chart.render();
+
 }
